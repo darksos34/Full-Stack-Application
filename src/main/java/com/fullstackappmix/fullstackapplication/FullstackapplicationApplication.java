bd3369eb63bfdbbed7eb158aab40c07832b3640b
Your Name <j.hamwijk34@gmail.com>
2020-07-29 12:35:44 +0200

    Update FullStackApp

---
+++ src/main/java/com/fullstackappmix/fullstackapplication/FullstackapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackappmix.fullstackapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullstackappmix/fullstackapplication/FullstackapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackappmix.fullstackapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
