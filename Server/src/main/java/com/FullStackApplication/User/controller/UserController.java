bd90d9931ea7b02c80556b7b6b103493feef8d40
Your Name <j.hamwijk34@gmail.com>
2020-07-30 11:13:37 +0200

    Upload, code needs to be modifed and tested
    Still in development

--- Server/src/main/java/com/FullStackApplication/User/controller/UserController.java
+++ Server/src/main/java/com/FullStackApplication/User/controller/UserController.java
@@ -2,8 +2,46 @@ package com.FullStackApplication.User.controller;
 
 // Request mapping
 // Restcontroller, link to acces GET,POST,PUT DELETE
-public class UserController {
-    //Id
-    //
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor // Lombok to create the Required Args Constructor
+@CrossOrigin(origins="http://localhost:3000", maxAge = 3600) // Cross origin allowed for port 3000
+@RestController // Instruct Spring Boot that this is the REST Controller
+@RequestMapping("/api/users") // Link to access GET/POST/PUT/DELETE requests
+public class UserController {
+
+    // Import Methods from other Class
+    private final UserService userService; // Class from which methods are being imported
+
+    // Get all users
+    @GetMapping // GET request handler
+//    @PreAuthorize("hasRole('ADMIN')") // Access granted for Admin Role
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    // Get user by ID
+    @GetMapping("/{id}") // GET request handler by ID
+//    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')") // Access granted for all roles
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    // Update user
+    @PutMapping // PUT request handler
+//    @PreAuthorize("hasRole('ADMIN')") // Access granted for Admin Role
+    public void updateUser(@Valid @RequestBody User user) {
+        userService.updateUser(user);
+    }
+
+    // Delete user
+    @DeleteMapping("/{id}") // DELETE request handler by ID
+//    @PreAuthorize("hasRole('ADMIN')") // Access granted for Admin Role
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
--- Server/src/main/java/com/FullStackApplication/User/repository/UserRepository.java
+++ Server/src/main/java/com/FullStackApplication/User/repository/UserRepository.java
@@ -1,4 +1,23 @@
 package com.FullStackApplication.User.repository;
+@Repository // Instruct Spring Boot that this is the Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
-public class UserRepository {
+    Optional<User> getUserByUsername(String username); // Lookup provided username in the database
+
+    Boolean existsByUsername(String username); // Check username existence in the database
+
+    Boolean existsByEmail(String email); // Check email address existence in the database
+
+    User findByEmail(String email);
+
+
+    //Modified from here
+
+    // modify this file to your own code
+
+    @Transactional // let's all statements be executed on readonly data
+    @Modifying // lets query  execute INSERT, UPDATE, DELETE and DDL queries
+    @Query("UPDATE User u SET u.password = :password WHERE u.id = :userId") // updates a user's password
+    void updatePassword(@Param("password") String password, @Param("userId") Long userId);
+
 }
