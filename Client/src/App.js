634b22c0bfc7567fcc86f9f2261f33ae5574ad82
Your Name <j.hamwijk34@gmail.com>
2020-07-30 11:03:38 +0200

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Get/Post
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/darksos34/Full-Stack-Application"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Created by DarkSoS
         </a>
       </header>
     </div>
