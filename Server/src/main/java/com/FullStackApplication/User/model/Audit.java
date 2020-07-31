1e40ddb18aef2cf18f9744beb294d94113b500ca
Your Name <j.hamwijk34@gmail.com>
2020-07-31 11:27:28 +0200

    Debugg code

---
+++ Server/src/main/java/com/FullStackApplication/User/model/Audit.java
@@ -0,0 +1,13 @@
+/*
+ *
+ *  * Coding for fun created by Darksos34.
+ *  * Copyright (c) 2020.
+ *  * All rights reserved by Jordy Hamwijk
+ *  * Learning Computer Language.
+ *
+ */
+
+package com.FullStackApplication.User.model;
+
+public class Audit {
+}
