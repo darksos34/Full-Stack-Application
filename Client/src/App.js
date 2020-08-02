d55ee23356b4da4c2d8b30e8f31bb83eabeb8390
Your Name <j.hamwijk34@gmail.com>
2020-08-02 18:01:19 +0200

    Uploaded Navigation bar

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+        <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +20,7 @@ function App() {
         >
          Created by DarkSoS
         </a>
+
       </header>
     </div>
   );
